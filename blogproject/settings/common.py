a34765eb398d59a2fd80cb26870cd56cd4980f1f
yongfa <yongfa063@gmail.com>
2020-03-10 11:47:06 +0800

    关闭全文搜索

--- blogproject/settings/common.py
+++ blogproject/settings/common.py
@@ -137,16 +137,16 @@ PAGINATION_SETTINGS = {
 
 
 # 搜索设置
-HAYSTACK_CONNECTIONS = {
-    'default': {
-        'ENGINE': 'blog.elasticsearch2_ik_backend.Elasticsearch2IkSearchEngine',
-        'URL': '',
-        'INDEX_NAME': 'hellodjango_blog_tutorial',
-    },
-}
-HAYSTACK_SEARCH_RESULTS_PER_PAGE = 10
-HAYSTACK_SIGNAL_PROCESSOR = 'haystack.signals.RealtimeSignalProcessor'
-HAYSTACK_CUSTOM_HIGHLIGHTER = 'blog.utils.Highlighter'
+# HAYSTACK_CONNECTIONS = {
+#     'default': {
+#         'ENGINE': 'blog.elasticsearch2_ik_backend.Elasticsearch2IkSearchEngine',
+#         'URL': '',
+#         'INDEX_NAME': 'hellodjango_blog_tutorial',
+#     },
+# }
+# HAYSTACK_SEARCH_RESULTS_PER_PAGE = 10
+# HAYSTACK_SIGNAL_PROCESSOR = 'haystack.signals.RealtimeSignalProcessor'
+# HAYSTACK_CUSTOM_HIGHLIGHTER = 'blog.utils.Highlighter'
 
 LOGOUT_REDIRECT_URL = '/'
 LOGIN_REDIRECT_URL = '/'
