76fafba0f27486240d636b4054758614fb97646a
yongfa <yongfa063@gmail.com>
2020-03-10 13:09:15 +0800

    修改域名

--- blogproject/settings/production.py
+++ blogproject/settings/production.py
@@ -3,7 +3,7 @@ from .common import *
 SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 
 print('production')
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['blog.liequantech.com']
 # HAYSTACK_CONNECTIONS['default']['URL'] = 'http://hellodjango_blog_tutorial_elasticsearch:9200/'
